c00bc551585d7e487735c4227e0eecf518eba66d
Hendrik Helwich <hendrik@helwich.de>
2017-08-03 20:57:10 +0200

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   output: {
-    filename: 'floating-point-converter.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
