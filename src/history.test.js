12898d6d6b005053225e098626358fc275111a9d
Hendrik Helwich <hendrik@helwich.de>
2017-09-26 21:48:16 +0200

    Add test

--- src/history.test.js
+++ src/history.test.js
@@ -22,3 +22,23 @@ describe('urlToState', () => {
     expect(urlToState('~~~~~~3.14')).toEqual({ fStr: '~~~3.14', f64: true })
   })
 })
+
+describe('urlToState o stateToUrl', () => {
+  it('is identity', () => {
+    const testIdentity = fStr => {
+      expect(urlToState(stateToUrl({ fStr, f64: true }))).toEqual({ fStr, f64: true })
+      expect(urlToState(stateToUrl({ fStr, f64: false }))).toEqual({ fStr, f64: false })
+    }
+    testIdentity('')
+    testIdentity('3.14')
+    testIdentity('1')
+    testIdentity('-1')
+    testIdentity('PI * 13.37')
+    testIdentity('~')
+    testIdentity('~~')
+    testIdentity('~~~')
+    testIdentity('~3.14')
+    testIdentity('~~3.14')
+    testIdentity('~~~3.14')
+  })
+})
